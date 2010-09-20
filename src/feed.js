bd5afd2f45ebbc06a4dd822c66303c0a92631744
Paul Chavard <paul@chavard.net>
2010-09-20 10:16:51 +0200

    Jamfile update and starting some work on docs

--- src/feed.js
+++ src/feed.js
@@ -1,7 +1,8 @@
-
-// // //
-// Feed
-//
+/**
+ * Feed class is used as an ancestor to NavigationFeed and AcquisitionFeed it handles
+ * all the parsing
+ * @abstract Not really abstract as it's full fledged, but it should not be used directly
+ */
 OPDS.Feed = _.Class({
   initialize: function(browser){
     this.browser = browser || new OPDS.Support.Browser();
@@ -9,12 +10,27 @@ OPDS.Feed = _.Class({
   },
   
   extend: {
-    parseUrl: function(url, callback, browser, opts){
+    /**
+     * Parse the given url.
+     *
+     * If the resource at the give url is not an OPDS Catalog, this method will 
+     * try to find a linked catalog.
+     * If many are available it will take the first one with a priority given to 
+     * null rel or rel="related" catalogs.
+     *
+     * @param url [String] url to parse
+     * @param callback [Function]
+     * @param browser (see Feed.parseRaw)
+     * @param parserOpts parser options (unused at the moment)
+     * @see OPDS::Support::Browser
+     * @return [AcquisitionFeed, NavigationFeed, Entry, null] an instance of a parsed feed, entry or null
+     */
+    parseUrl: function(url, callback, browser, parserOpts){
       var browser = browser || new OPDS.Support.Browser();
       var self = this;
       browser.goTo(url, function(browser){
         if (browser.isOk()) {
-          var parsed = self.parseRaw(browser.body(), opts, browser);
+          var parsed = self.parseRaw(browser.body(), parserOpts, browser);
           if (parsed == null) {
             var disco = browser.discover(browser.currentLocation, function(){
               if (disco.size > 0) {
@@ -34,7 +50,14 @@ OPDS.Feed = _.Class({
         }
       });
     },
-
+    /**
+     * Will parse a text stream as an OPDS Catalog, internaly used by #parseUrl
+     *
+     * @param txt [String] text to parse
+     * @param opts [Hash] options to pass to the parser
+     * @param browser [OPDS.Support.Browser] an optional compatible browser to use
+     * @return [AcquisitionFeed, NavigationFeed] an instance of a parsed feed or null
+     */
     parseRaw: function(txt, opts, browser){
       var parser = new OPDS.Parser(opts);
       return parser.parse(txt, browser);
@@ -48,13 +71,20 @@ OPDS.Feed = _.Class({
     }
   },
 
-  // read xml entries into entry struct
+  /**
+   * @private
+   * read xml entries into the entry list struct
+   */
   serialize: function(){
     // Id
     this.id = this.rawDoc.find('feed>id').text();
-    // Icon
+    /**
+      @return [String] Feed icon definition
+     */
     this.icon = this.rawDoc.find('feed>icon').text();
-    // Title
+    /**
+      @return [String] Feed title
+     */
     this.title = this.rawDoc.find('feed>title').text();
     // Author
     this.author = {
@@ -102,12 +132,22 @@ OPDS.Feed = _.Class({
   }
 });
 
-// // //
-// NavigationFeed
-//
-OPDS.NavigationFeed = _.Class(OPDS.Feed, {});
+/**
+ * Represents a navigation feed 
+ * @see http://opds-spec.org/specs/opds-catalog-1-0-20100830/#Navigation_Feeds
+ */
+OPDS.NavigationFeed = _.Class(OPDS.Feed, {
+  /**
+   * Collection of all Navigation feeds found in this feed
+   * @return [OPDS::Support::LinkSet] found links
+   */
+  navigationLinks: function(){
+    
+  }
+});
 
-// // //
-// AcquisitionFeed
-//
+/**
+ * Represents an acquisition feed
+ * @see http://opds-spec.org/specs/opds-catalog-1-0-20100830/#Acquisition_Feeds
+ */
 OPDS.AcquisitionFeed = _.Class(OPDS.Feed, {});
